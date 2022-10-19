cd7cb9704090e0ad39101a7e7163034cacc54694
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-10-19 21:21:51 +0700

    membuat skeleton loading

---
+++ src/components/Home/SkeletonLoading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SkeletonLoading() {
+  return (
+    <div className="`w-full h-32 rounded-lg shadow-xl animate-pulse bg-primaryGrey"></div>
+  );
+}
+
+export default SkeletonLoading;
