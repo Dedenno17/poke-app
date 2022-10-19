d5f1315fc2d70baf7400af8ccf0ad6dd9ba0b25d
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-10-19 10:54:35 +0700

    membuat Homecomponent & menambahkan bg untuk page home

---
+++ src/components/Home/HomeComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomeComponent() {
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 pt-10 px-4">
+      <h1 className="text-4xl text-primaryBlack font-semibold">Pokedex</h1>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import HomeComponent from "../components/Home/HomeComponent";
+import pokeball from "../assets/pokeball.png";
+
 function Home() {
-  return <div className="text-5xl text-green-500 font-semibold">Home</div>;
+  return (
+    <div className="bg-primaryWhite w-full min-h-[100vh] relative">
+      <img
+        src={pokeball}
+        alt="pokeball"
+        className="absolute w-64 h-64 z-0 -top-36 -right-24 opacity-60"
+      />
+      <HomeComponent />
+    </div>
+  );
 }
 
 export default Home;
