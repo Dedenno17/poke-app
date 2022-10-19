e4bd64b40f9bd87063977a23dd38165de843b48f
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-10-19 09:41:02 +0700

    membuat variable colors di tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryWhite: "#f6f6f6",
+        primaryBlack: "#313942",
+        primaryGrey: "#dcdee0",
+      },
+    },
   },
   plugins: [],
 };
