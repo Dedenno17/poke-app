ec7d48a13e0fb9846b88e154f52c9c7e3c91ee83
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-10-19 21:29:47 +0700

    menambahkan jumlah skeleton loading item

--- src/components/Home/PokemonList.jsx
+++ src/components/Home/PokemonList.jsx
@@ -50,9 +50,9 @@ function PokemonList(props) {
         ))}
       {pokemonsData.length === 0 &&
         props.pokemonList.length === 0 &&
-        [1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
-          <SkeletonLoading key={Math.random() + item + ""} />
-        ))}
+        [
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ].map((item) => <SkeletonLoading key={Math.random() + item + ""} />)}
     </div>
   );
 }
