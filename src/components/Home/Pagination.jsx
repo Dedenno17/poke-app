39a9788c748841cb54e5c1843933e138db57b8e9
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-10-19 14:35:14 +0700

    memberi style pada pagination

--- src/components/Home/Pagination.jsx
+++ src/components/Home/Pagination.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import angleLeft from "../../assets/angle-left-solid.svg";
+import angleٌRight from "../../assets/angle-right-solid.svg";
+
 export default function Pagination() {
   return (
-    <div className="w-full">
-      <div></div>
+    <div className="w-full flex items-center justify-around py-4">
+      <div className="flex p-4 bg-primaryGrey rounded-lg shadow-xl w-[30%]">
+        <img src={angleLeft} alt="arrow left" className="w-[15%] m-auto" />
+      </div>
+      <div className="flex p-4 bg-primaryGrey rounded-lg shadow-xl w-[30%]">
+        <img src={angleٌRight} alt="arrow left" className="w-[15%] m-auto" />
+      </div>
     </div>
   );
 }
