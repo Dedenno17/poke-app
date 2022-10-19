d89c176361d5f0337b8fc7033fc776f2bd03b9b9
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-10-19 10:34:19 +0700

    membuat route untuk menghubungkan home & detail di App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Detail />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:name" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
